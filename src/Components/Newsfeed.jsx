b48341ad1eaf413ec2f2805bf53b24ebeb6bccaa
Subham-Abhishek <abhi950727@gmail.com>
2021-09-30 16:01:28 +0530

    Lazy Loading added

--- src/Components/Newsfeed.jsx
+++ src/Components/Newsfeed.jsx
@@ -1,29 +1,49 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import phgif from "../img/phgif.gif";
+import { ImagePlaceholder } from "./ImagePlaceholder";
 import classes from "./newsfeed.module.css";
 
 export const Newsfeed = () => {
   const [list, setList] = useState([]);
   const [query, setQuery] = useState("random");
-  const placeholder = [];
+  const [load, setLoad] = useState(true);
 
   const url = `https://api.unsplash.com/search/photos?page=1&per_page=30&query=${query}&client_id=KnIdKmvxNCmKWEiC6BUzyQtUnIryKv1Cv53bbTc9ahU`;
 
   const getImage = () => {
-      axios.get(url).then(({data: {results}}) => {
-          console.log(results);
-          setList(results);
-      })
-  }
+    axios.get(url).then(({ data: { results } }) => {
+      console.log(results);
+      setList(results);
+      setLoad(false);
+    });
+  };
 
   useEffect(() => {
-    getImage();
-  }, []);
-console.log(list);
-  return <div className={classes.newsfeed}>
-      {list.map((e) => {
-          return <img src={e.urls.thumb} alt="" />
-      })}
-  </div>;
+    setTimeout(() => {
+      getImage();
+    }, 2000);
+  }, [query]);
+
+  return (
+    <div className={classes.newsfeed}>
+      {load ? "Loading" : (
+        list.map((e) => {
+          return (
+            <LazyLoadImage
+              effect="blur"
+              src={e.urls.regular}
+              alt={e.alt_description}
+              key={e.id}
+              height={Math.floor(e.height / 10)}
+              width="100%"
+            />
+          );
+        })
+      )}
+    </div>
+  );
 };
