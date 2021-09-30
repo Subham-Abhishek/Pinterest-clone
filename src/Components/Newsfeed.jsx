65bbb265f944db6e68370435f8e254f49f5fa712
Subham-Abhishek <abhi950727@gmail.com>
2021-09-30 23:47:12 +0530

    infinite scrolling improved

--- src/Components/Newsfeed.jsx
+++ src/Components/Newsfeed.jsx
@@ -5,81 +5,61 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { ImagePlaceholder } from "./ImagePlaceholder";
 import classes from "./newsfeed.module.css";
-
-let bool = true;
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export const Newsfeed = () => {
-  console.log("newsfeed");
+  const [lists, setList] = useState([]);
   const [query, setQuery] = useState("");
   const [pageNumber, setPageNumber] = useState(1);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-  const [lists, setList] = useState([]);
+
+  const fetchImages = () => {
+    axios({
+      method: "GET",
+      url: "https://api.unsplash.com/search/photos",
+      params: {
+        page: pageNumber,
+        per_page: 25,
+        query: query || "random",
+        client_id: "KnIdKmvxNCmKWEiC6BUzyQtUnIryKv1Cv53bbTc9ahU",
+      },
+    }).then(({ data }) => {
+      setPageNumber(pageNumber+1);
+      setList((prev) => {
+        return [...prev, ...data.results];
+      });
+      setLoading(false);
+    });
+  };
 
   useEffect(() => {
-    setList([]);
-  }, [query]);
+    fetchImages();
+  }, []);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(true);
-      setError(false);
-      let cancel;
-      axios({
-        method: "GET",
-        url: "https://api.unsplash.com/search/photos",
-        params: {
-          page: pageNumber,
-          per_page: 25,
-          query: query || "random",
-          client_id: "KnIdKmvxNCmKWEiC6BUzyQtUnIryKv1Cv53bbTc9ahU",
-        },
-        cancelToken: new axios.CancelToken((c) => (cancel = c)),
-      })
-        .then(({ data }) => {
-          setList((prev) => {
-            return [...prev, ...data.results];
-          });
-          setLoading(false);
-          bool = true;
-        })
-        .catch((e) => {
-          console.log("error1");
-          if (axios.isCancel(e)) return;
-          console.log("cancelerror1");
-          setError(true);
-        });
-      return () => cancel();
-    }, 500);
-  }, [query, pageNumber]);
-
-  window.addEventListener("scroll", () => {
-    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-
-    if (scrollTop + clientHeight >= scrollHeight && bool) {
-      console.log("call again");
-      bool = false;
-      setPageNumber((prev) => prev + 1);
-    }
-  });
+  console.log(lists);
+  console.log(pageNumber);
 
   return (
     <div className={classes.newsfeed}>
-      {loading ? (
-        <>
-          <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-          <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-          <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-          <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-          <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-          <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-          <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-          <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-          <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-        </>
-      ) : (
-        lists.map((list, index) => {
-          if (list.length === index + 1) {
+      <InfiniteScroll
+        dataLength={lists.length}
+        next={fetchImages}
+        hasMore={lists.length > 0}
+      >
+        {loading ? (
+          <>
+            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
+            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
+            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
+            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
+            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
+            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
+            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
+            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
+            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
+          </>
+        ) : (
+          lists.map((list) => {
             return (
               <LazyLoadImage
                 effect="blur"
@@ -90,22 +70,9 @@ export const Newsfeed = () => {
                 width="100%"
               />
             );
-          } else {
-            return (
-              <LazyLoadImage
-                effect="blur"
-                key={list.id}
-                src={list.urls.regular}
-                alt={list.alt_description}
-                height={Math.floor(list.height / 15)}
-                width="100%"
-              />
-            );
-          }
-        })
-      )}
-      <div>{loading && "Loading..."}</div>
-      <div>{error && "Error"}</div>
+          })
+        )}
+      </InfiniteScroll>
     </div>
   );
 };
