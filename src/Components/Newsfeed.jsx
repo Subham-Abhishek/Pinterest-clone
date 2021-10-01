f44541aad049f82e5096087b5e1cbd6e25ab3021
Subham-Abhishek <abhi950727@gmail.com>
2021-10-01 14:28:13 +0530

    News Feed page done

--- src/Components/Newsfeed.jsx
+++ src/Components/Newsfeed.jsx
@@ -6,6 +6,8 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 import { ImagePlaceholder } from "./ImagePlaceholder";
 import classes from "./newsfeed.module.css";
 import InfiniteScroll from "react-infinite-scroll-component";
+import styled from "styled-components";
+import UploadIcon from "@mui/icons-material/Upload";
 
 export const Newsfeed = () => {
   const [lists, setList] = useState([]);
@@ -24,7 +26,8 @@ export const Newsfeed = () => {
         client_id: "KnIdKmvxNCmKWEiC6BUzyQtUnIryKv1Cv53bbTc9ahU",
       },
     }).then(({ data }) => {
-      setPageNumber(pageNumber+1);
+      console.log(data);
+      //   setPageNumber(pageNumber + 1);
       setList((prev) => {
         return [...prev, ...data.results];
       });
@@ -33,46 +36,169 @@ export const Newsfeed = () => {
   };
 
   useEffect(() => {
-    fetchImages();
+    setTimeout(() => {
+      //   fetchImages();
+    }, 1000);
   }, []);
 
   console.log(lists);
   console.log(pageNumber);
 
   return (
-    <div className={classes.newsfeed}>
-      <InfiniteScroll
-        dataLength={lists.length}
-        next={fetchImages}
-        hasMore={lists.length > 0}
-      >
-        {loading ? (
-          <>
-            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-            <ImagePlaceholder height={Math.floor(Math.random() * 600 + 200)} />
-          </>
-        ) : (
-          lists.map((list) => {
-            return (
-              <LazyLoadImage
-                effect="blur"
-                key={list.id}
-                src={list.urls.regular}
-                alt={list.alt_description}
-                height={Math.floor(list.height / 15)}
-                width="100%"
+    <div className={classes.newsparent}>
+      <div className={classes.newsfeed}>
+        <InfiniteScroll
+          dataLength={lists.length}
+          //   next={fetchImages}
+          //   hasMore={lists.length > 0}
+        >
+          {loading ? (
+            <>
+              <ImagePlaceholder
+                height={Math.floor(Math.random() * 400 + 150)}
               />
-            );
-          })
-        )}
-      </InfiniteScroll>
+              <ImagePlaceholder
+                height={Math.floor(Math.random() * 400 + 150)}
+              />
+              <ImagePlaceholder
+                height={Math.floor(Math.random() * 400 + 150)}
+              />
+              <ImagePlaceholder
+                height={Math.floor(Math.random() * 400 + 150)}
+              />
+              <ImagePlaceholder
+                height={Math.floor(Math.random() * 400 + 150)}
+              />
+              <ImagePlaceholder
+                height={Math.floor(Math.random() * 400 + 150)}
+              />
+              <ImagePlaceholder
+                height={Math.floor(Math.random() * 400 + 150)}
+              />
+              <ImagePlaceholder
+                height={Math.floor(Math.random() * 400 + 150)}
+              />
+              <ImagePlaceholder
+                height={Math.floor(Math.random() * 400 + 150)}
+              />
+              <ImagePlaceholder
+                height={Math.floor(Math.random() * 400 + 150)}
+              />
+            </>
+          ) : (
+            lists.map((list) => {
+              return (
+                <PinCard key={list.id}>
+                  <LazyLoadImage
+                    effect="blur"
+                    src={list.urls.regular}
+                    alt={list.alt_description}
+                    height={Math.floor(list.height / 15)}
+                    width="100%"
+                  />
+                  <div className="savebtn">Save</div>
+                  <div className="bottom">
+                    <div className="descript">
+                      {list.user.name.slice(0, 10)}
+                    </div>
+                    <div className="send">
+                      <UploadIcon />
+                    </div>
+                  </div>
+                  <div className="avatar">
+                    <img
+                      src={`https://joeschmoe.io/api/v1/${list.user.username}`}
+                      alt="User Avatar"
+                    />
+                  </div>
+                </PinCard>
+              );
+            })
+          )}
+        </InfiniteScroll>
+      </div>
     </div>
   );
 };
+
+const PinCard = styled.div`
+  position: relative;
+  img {
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    &:hover {
+      cursor: zoom-in;
+      filter: brightness(50%);
+    }
+  }
+  .avatar {
+    img {
+      position: absolute;
+      width: 30px;
+      height: 30px;
+      margin-left: 5px;
+      cursor: default;
+      border-radius: 50%;
+      box-shadow: 2px 2px 10px lightgrey;
+      z-index: 1000;
+      top: 10px;
+      left: 10px;
+      display: none;
+      cursor: pointer;
+      filter: brightness(100%);
+    }
+  }
+  &:hover {
+    .savebtn,
+    .descript,
+    .send,
+    .avatar img {
+      display: block;
+    }
+  }
+  .savebtn {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    color: white;
+    background-color: red;
+    padding: 10px 15px;
+    font-weight: 600;
+    border-radius: 25px;
+    cursor: pointer;
+    display: none;
+    &:hover {
+      background-color: #970505;
+    }
+  }
+  .descript {
+    position: absolute;
+    bottom: 20px;
+    left: 10px;
+    background-color: #ffffffd8;
+    font-size: 14px;
+    font-weight: 700;
+    padding: 4px 10px;
+    border-radius: 20px;
+    display: none;
+  }
+  .send {
+    position: absolute;
+    bottom: 18px;
+    right: 10px;
+    background-color: #ffffffd8;
+    padding: 3px 5px;
+    border-radius: 50%;
+    display: none;
+    cursor: pointer;
+    &:hover {
+      background-color: #fffffff8;
+    }
+    svg {
+      font-size: 20px;
+    }
+  }
+`;
