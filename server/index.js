6e9ee77ce9a5e2b96f8d5ea0c2501d2fd41bd46f
Subham-Abhishek <abhi950727@gmail.com>
2021-10-17 20:19:32 +0530

    deployed backend server

--- server/index.js
+++
@@ -1,3 +0,0 @@
-const start = require("./src/server");
-// console.log("helloo from index")
-start()
--- server/src/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const {MONGOURI}=require('../keys')
-const connect=()=>{
-    return mongoose.connect(MONGOURI)
-}
-module.exports = connect
