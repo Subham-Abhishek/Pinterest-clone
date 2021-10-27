6e561b1acd15d056603198ca37e1e3a1f2db88c1
Avinash7564 <avinashkumarmbns@gmail.com>
2021-10-27 22:23:39 +0530

    minor changes

--- src/context/TokenProvider.jsx
+++ src/context/TokenProvider.jsx
@@ -3,20 +3,17 @@ import React, { useState, createContext } from "react";
 export const TokenContext = createContext();
 
 export const TokenProvider = ({ children }) => {
-  const [token, setToken] = useState(localStorage.getItem("token") || "");
+
+
+  const [token, setToken] = useState("");
   const [query, setQuery] = useState("");
-  const [gUser, setGUser] = useState(
-    JSON.parse(localStorage.getItem("gUser")) || null
-  );
-  console.log(
-    `token: ${token}, ----query----${query}, -----gUser-----${gUser}`
-  );
+  const [gUser, setGUser] = useState();
+  console.log(`token: ${token}, ----query----${query}, -----gUser-----${gUser}`);
 
   return (
-    <TokenContext.Provider
-      value={{ token, setToken, query, setQuery, gUser, setGUser }}
-    >
+    <TokenContext.Provider value={{ token, setToken, query, setQuery, gUser, setGUser }}>
       {children}
     </TokenContext.Provider>
+    
   );
 };
